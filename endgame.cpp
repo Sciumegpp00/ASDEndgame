858a0fb2a9dc5944f8ddec49f3493063d9b357fb
Alessandro Zanola <ales.zanola@gmail.com>
2021-05-27 19:44:39 +0200

    Integrate endgame library and init lower bound algorithm

--- endgame.cpp
+++ endgame.cpp
@@ -1,9 +1,17 @@
+#define DEBUG
+
 #include <iostream>
 #include <stdlib.h>
-#include "endgame.h"
+#include "lib/endgame.h"
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include <set>
+
+#define INPUT_FILE "inputs/input0.txt"
+#define OUTPUT_FILE "outputs/output0.txt"
+
+using namespace std;
 
 // @tocompile /usr/bin/g++ -DEVAL -std=c++11 -O2 -pipe -static -s -o endgame grader.cpp endgame.cpp
 // problem with compiling with Clion --> modify cMakeList.txt
@@ -11,8 +19,8 @@ struct Rock {
     int mass;
     int energy;
 
-    void printRock(ofstream* out){
-        *out << mass << " " <<  energy << endl;
+    void printRock(ofstream *out) {
+        *out << mass << " " << energy << endl;
     }
 };
 
@@ -20,18 +28,20 @@ struct City {
     int n;
     vector<Rock> rocks;
 
-    void printCity(ofstream* out){
+    void printCity(ofstream *out) {
         *out << n << endl;
-        for (int i = 0; i < rocks.size(); ++i) {
-            *out << rocks[i].mass << " " <<  rocks[i].energy;
-        }
-        *out << endl;
     }
 };
 
+//struct Edge {
+//    int weight;
+//    short c1;
+//    short c2;
+//};
 
-int nCitiesTot;
-int nDifferentRocks;
+
+short int nCitiesTot;
+short int nDifferentRocks;
 int nTakenRocks;
 int source;
 int C;
@@ -41,16 +51,91 @@ double vmin;
 Rock *rocks;
 City *cities;
 int **weights;
+//int edgeCounts;
+//Edge* edges;
 
-int getWeight(int city1, int city2) { //city 2 is the line and city 1 is the arrival city (bigger)
+int getWeight(short city1, short city2);
+void input();
+void output();
+void bbTps();
+
+//int edgeCmp(Edge x, Edge y) {
+//    return x.weight < y.weight
+//           ? -1
+//           : x.weight > y.weight;
+//}
+
+int main() {
+    input();
+    /*
+    auto err = heapsort(edges, sizeof(Edge), edgeCounts, reinterpret_cast<int (*)(const void *, const void *)>(&edgeCmp));
+    if(err == -1) {
+        cout << "Error in heapsort!\n";
+    }*/
+    bbTps();
+
+    output();
+    return 0;
+}
+
+void removeVector(vector<short>* v, short x) {
+    for (auto it = v->begin() ; it != v->end(); ++it) {
+        if(*it == x)
+            v->erase(it);
+    }
+}
+
+int calcLb(short origin, vector<short>* choices, int cost) {
+    int lb, out, back, *transfer, costLocal;
+    short outCity;
+
+    if(choices->empty())
+        return 0; // FIXME: Add cost to come back
+
+    //calculate minimum cost of exiting edges at this point
+    out = getWeight(origin, (*choices)[0]);
+    outCity = 0;
+    for (int i = 1; i < choices->size(); i++) {
+        costLocal = getWeight(origin, (*choices)[i]);
+        if(costLocal < out) {
+            out = costLocal;
+            outCity = (*choices)[i];
+        }
+    }
+    //calculate lb cost to go back
+
+    //calculate min future path
+
+    //lb = cost + (out+ba)
+    return 0;
+}
+
+void bbTps(vector<short>* path, int cost, vector<short>* leftChoices, int n, int i) {
+    vector<short> choices = *leftChoices;
+    int out, back;
+
+    for (int i = 0; i < nCitiesTot; i++) {
+
+    }
+
+    for (auto it = choices.begin() ; it != choices.end(); ++it){
+        path->push_back(*it);
+        removeVector(leftChoices, *it);
+
+        if(i < n){
+            auto lb = calcLb(*it, &choices, cost);
+        }
+    }
+}
+
+int getWeight(short city1, short city2) { //city 2 is the line and city 1 is the arrival city (bigger)
     return city1 < city2
            ? weights[city2 - 1][city1]
            : weights[city1 - 1][city2];
 }
 
 void input() {
-    ifstream in("input0.txt");
-    ofstream out("output.txt");
+    ifstream in(INPUT_FILE);
 
     in >> nCitiesTot >> source;
     in >> nDifferentRocks >> C >> R >> vmin >> vmax;
@@ -60,7 +145,7 @@ void input() {
 
     for (int i = 0; i < nDifferentRocks; ++i) {
         in >> rocks[i].mass >> rocks[i].energy; //mass and energy for each rock
-        //rocks[i].printRock(&out);
+        //rocks[i].printRock(&cout);
     }
 
     int rocksPerCity;
@@ -70,35 +155,41 @@ void input() {
     for (int i = 0; i < nDifferentRocks; ++i) { //i-th rock
         in >> rocksPerCity;
 
-        //out << rocksPerCity << endl;
+        //cout << rocksPerCity << endl;
         for (int j = 0; j < rocksPerCity; ++j) {
             in >> city;
 
             cities[city].rocks.push_back(rocks[i]); //push(number of rock)
-            //out << city << " ";
+            //cout << city << " ";
         }
     }
 
     weights = new int *[nCitiesTot - 1]; //1 is 0
 
-    for (int i = 1; i < nCitiesTot; ++i) {
+//    edgeCounts = nCitiesTot * (nCitiesTot - 1) / 2;
+//    edges = new Edge[edgeCounts];
+
+    for (short i = 1; i < nCitiesTot; ++i) {
         weights[i] = new int[i];
-        for (int j = 0; j < i; ++j) {
+        for (short j = 0; j < i; ++j) {
             in >> weights[i][j];
-            //out << weights[i][j] << " ";
+//            edges[(i * (i - 1) / 2) + j].weight = weights[i][j];
+//            edges[(i * (i - 1) / 2) + j].c1 = i;
+//            edges[(i * (i - 1) / 2) + j].c2 = j;
+            //cout << weights[i][j] << " ";
         }
-        //out << endl;
+        //cout << endl;
     }
 }
 
-//@todo adapt to new structures
-void output(double E, double G, double T, int *rocks, int *cities) {
-    ofstream out("output.txt");
+// TODO fix total variables
+void output() {
+    ofstream out(OUTPUT_FILE);
 
     //final glove's energy, rocks' energy and time for the journey
-    out << scientific << setprecision(10) << E << " ";
-    out << scientific << setprecision(10) << G << " ";
-    out << scientific << setprecision(10) << T << endl;
+    out << scientific << setprecision(10) << E << " "; //total glove's energy
+    out << scientific << setprecision(10) << G << " "; //total rocks enrgy
+    out << scientific << setprecision(10) << T << endl; //time used
 
     //print all taken rocks
     for (int i = 0; i < nTakenRocks; i++) {
@@ -108,19 +199,11 @@ void output(double E, double G, double T, int *rocks, int *cities) {
 
     //print the path (ordered, t0 = tN = source)
     for (int i = 0; i < nCitiesTot; i++) {
-        printf("%d ", cities[i]);
+        cities[i].printCity(&out);
     }
     printf("\n***\n");
 }
 
 int calcV(int vMax, int vMin, int W, int C) {
     return vmax - W * ((vMax - vMin) / C);
-}
-
-int main() {
-    int *rocks, cities;
-    input();
-
-
-    return 0;
 }
