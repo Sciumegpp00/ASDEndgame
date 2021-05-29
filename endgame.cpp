80f983711f9840dd0e212a4c8810160bdf4ac0f4
Alessandro Zanola <ales.zanola@gmail.com>
2021-05-29 13:27:00 +0200

    Complete first version of bbTps

--- endgame.cpp
+++ endgame.cpp
@@ -13,8 +13,6 @@
 
 using namespace std;
 
-// @tocompile /usr/bin/g++ -DEVAL -std=c++11 -O2 -pipe -static -s -o endgame grader.cpp endgame.cpp
-// problem with compiling with Clion --> modify cMakeList.txt
 struct Rock {
     int mass;
     int energy;
@@ -25,12 +23,7 @@ struct Rock {
 };
 
 struct City {
-    int n;
     vector<Rock> rocks;
-
-    void printCity(ofstream *out) {
-        *out << n << endl;
-    }
 };
 
 //struct Edge {
@@ -39,11 +32,17 @@ struct City {
 //    short c2;
 //};
 
+int getWeight(short city1, short city2);
+void input();
+void output();
+void bbTsp(vector<short>* path, int cost, vector<short>* remaining, int n, int i);
+void removeVector(vector<short>* v, short x);
 
-short int nCitiesTot;
-short int nDifferentRocks;
+
+short nCitiesTot;
+short nDifferentRocks;
 int nTakenRocks;
-int source;
+short source;
 int C;
 double R;
 double vmax;
@@ -51,13 +50,11 @@ double vmin;
 Rock *rocks;
 City *cities;
 int **weights;
+int minCost; //costo di un percorso minimo a caso (il primo percorso che prendo)
+vector<short>* minSol = nullptr;
+vector<short>* remainingBbTsp = nullptr;
 //int edgeCounts;
 //Edge* edges;
-
-int getWeight(short city1, short city2);
-void input();
-void output();
-void bbTps();
 
 //int edgeCmp(Edge x, Edge y) {
 //    return x.weight < y.weight
@@ -72,84 +69,13 @@ int main() {
     if(err == -1) {
         cout << "Error in heapsort!\n";
     }*/
-    bbTps();
+
+    bbTsp(minSol, 0, remainingBbTsp, nCitiesTot - 1, 1);
+
+    
 
     output();
     return 0;
-}
-
-void removeVector(vector<short>* v, short x) {
-    for (auto it = v->begin() ; it != v->end(); ++it) {
-        if(*it == x)
-            v->erase(it);
-    }
-}
-
-int calcLb(short origin, vector<short>* choices, int cost) {
-    int lb, out, back, *transfer, costLocal;
-    short outCity;
-    double minLb;
-
-    if(choices->empty())
-        return 0; // FIXME: Add cost to come back
-
-    //calculate minimum cost of exiting edges at this point
-    out = getWeight(origin, (*choices)[0]);
-    outCity = 0;
-    for (int i = 1; i < choices->size(); i++) {
-        costLocal = getWeight(origin, (*choices)[i]);
-        if(costLocal < out) {
-            out = costLocal;
-            outCity = (*choices)[i];
-        }
-    }
-
-    //calculate lb cost to go back to source of the nearest cities
-    back = getWeight(origin, (*choices)[0]);
-    for (int i = 1; i < choices->size(); i++) {
-        costLocal = getWeight((*choices)[i], (*choices)[0]);
-        if(costLocal < back) {
-            back = costLocal;
-        }
-    }
-
-    //calculate min future path
-    minLb = cost + ((out+back + transfer[0])/2);
-    for (int i = 1; i < nCitiesTot; i++) {
-        lb = cost + ((out+back + transfer[i])/2);
-        if(lb < minLb) minLb = lb;
-    }
-
-    return minLb;
-}
-
-void bbTsp(vector<short>* path, int cost, vector<short>* leftChoices, int n, int i) {
-    vector<short> choices = *leftChoices;
-    vector<short> minSol;
-    int minCost = 100000000; //FIXME: initial cost
-    int lb;
-
-    for (auto it = choices.begin() ; it != choices.end(); ++it){
-        path->push_back(*it);
-        removeVector(leftChoices, *it);
-
-        if(i < n){
-            lb = calcLb(*it, &choices, cost);
-            if(lb < minCost){
-                bbTsp(path, cost + getWeight((short) *(it-1), (short) *it), leftChoices, n, i+1);
-            }
-        }else{
-            cost += getWeight((short) *(it), (short) (*path)[0]); //path->front() Returns a reference to the first element in the vector.
-            vector<short> minSol = choices;
-            minCost = lb;
-        }
-    }
-}
-
-int getWeight(short city1, short city2) { //city 2 is the line and city 1 is the arrival city (bigger)
-    return city1 < city2
-           ? weights[city2 - 1][city1]
-           : weights[city1 - 1][city2];
 }
 
 void input() {
@@ -172,25 +98,39 @@ void input() {
 
     for (int i = 0; i < nDifferentRocks; ++i) { //i-th rock
         in >> rocksPerCity;
-
         //cout << rocksPerCity << endl;
+
         for (int j = 0; j < rocksPerCity; ++j) {
             in >> city;
 
-            cities[city].rocks.push_back(rocks[i]); //push(number of rock)
+            cities[city].rocks.push_back(rocks[i]);
             //cout << city << " ";
         }
     }
 
     weights = new int *[nCitiesTot - 1]; //1 is 0
+    remainingBbTsp = new vector<short>(nCitiesTot - 1);
 
 //    edgeCounts = nCitiesTot * (nCitiesTot - 1) / 2;
 //    edges = new Edge[edgeCounts];
+    minSol = new vector<short>(nCitiesTot);
+    minSol->push_back(source);
+    minCost = 0;
 
     for (short i = 1; i < nCitiesTot; ++i) {
+        if(i != source) {
+            remainingBbTsp->push_back(i);
+        }
+
         weights[i] = new int[i];
         for (short j = 0; j < i; ++j) {
             in >> weights[i][j];
+
+            //creating a minimum path (random)
+            if(j == minSol->back()){ //if we're considering the distance between the previous point and another (anyone)
+                minCost += weights[i][j]; //sum the cost to the minimum
+                minSol->push_back(i); //add the new node in the solution
+            }
 //            edges[(i * (i - 1) / 2) + j].weight = weights[i][j];
 //            edges[(i * (i - 1) / 2) + j].c1 = i;
 //            edges[(i * (i - 1) / 2) + j].c2 = j;
@@ -222,6 +162,86 @@ void input() {
     printf("\n***\n");
 }*/
 
+int calcLb(short origin, vector<short>* remaining, int cost) {
+    int out, back, transfer, costLocal;
+
+    out = getWeight(origin, (*remaining)[0]);
+    back = getWeight((*remaining)[0], source);
+//    transfer = out + getWeight((*remainingBbTsp)[0], (*remainingBbTsp)[1]);
+    for (int i = 1; i < remaining->size(); i++) {
+        // calculate minimum cost of exiting edges at this point
+        costLocal = getWeight(origin, (*remaining)[i]);
+        if(costLocal < out) {
+            out = costLocal;
+        }
+
+        // Calculate for each city the best way to pass into it (best duo entry + exit)
+        // origin --> qualsiasi nodo + qualsiasi nodo
+//        int bestExit = weights[(*remainingBbTsp)[i]][(*remainingBbTsp)[i-1]];
+//        for (int j = 0; j < remainingBbTsp->size(); j++){ //consider the remainingBbTsp nodes
+//            if(i != j){
+//
+//            }
+//        }
+
+        //calculate lb cost to go back to source of the nearest cities
+        costLocal = getWeight((*remaining)[i], source);
+        if(costLocal < back) {
+            back = costLocal;
+        }
+    }
+
+    // TODO: Try to remove /2 for remainingBbTsp nodes > 10 or 15 for example
+    return cost + out + back;
+}
+
+
+void bbTsp(vector<short>* path, int cost, vector<short>* remaining, int n, int i) {
+    short origin = path->back();
+    vector<short> choices = *remaining;
+    int lb, arriveCost;
+
+    for (auto it = choices.begin() ; it != choices.end(); ++it) {
+        path->push_back(*it);
+        removeVector(remaining, *it);
+
+        if(i < n) {
+            arriveCost = getWeight(origin, *it);
+            lb = calcLb(*it, &choices, cost + arriveCost);
+            if(lb < minCost){
+                bbTsp(path, cost + arriveCost, remaining, n, i + 1);
+            }
+        } else {
+            cost += getWeight(*it, (*path)[0]); //path->front() Returns a reference to the first element in the vector.
+            if(minSol) {
+                delete minSol;
+                minSol = nullptr;
+            }
+            minSol = new vector<short>(choices);
+            minCost = lb;
+        }
+
+        remaining->push_back(*it);
+    }
+}
+
+int getWeight(short city1, short city2) { //city 2 is the line and city 1 is the arrival city (bigger)
+    return city1 < city2
+           ? weights[city2 - 1][city1]
+           : weights[city1 - 1][city2];
+}
+
 int calcV(int vMax, int vMin, int W, int C) {
     return vmax - W * ((vMax - vMin) / C);
+}
+
+int calcEnergy(int gloveEnergy, int totalTime){
+    return gloveEnergy - R * totalTime;
+}
+
+void removeVector(vector<short>* v, short x) {
+    for (auto it = v->begin() ; it != v->end(); ++it) {
+        if(*it == x)
+            v->erase(it);
+    }
 }
